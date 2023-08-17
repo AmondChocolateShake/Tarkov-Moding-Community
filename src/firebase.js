cddca008ea9bb879f3f6851e35e5253d98aaf3e5
AmondChocolateShake <dongju230130@gmail.com>
2023-08-17 13:38:36 +0900

    a

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCQHyHVJriwsPRf8XXv7JnC3DxVxKHrej4",
-  authDomain: "project-a-cc8b8.firebaseapp.com",
-  projectId: "project-a-cc8b8",
-  storageBucket: "project-a-cc8b8.appspot.com",
-  messagingSenderId: "852220573897",
-  appId: "1:852220573897:web:ccddedbc75dadbfd63b518",
-  measurementId: "G-G4E9P4LJTD"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
