3cb00b0b4f553e0c4cfca5b96e18a80779a892a4
AmondChocolateShake <dongju230130@gmail.com>
2023-08-20 02:38:34 +0900

    코드 누가 짰냐 개팬다 진짜 #5 리액트 상태값 전달 똑바로 해야함. 리덕스 설치했으니 제대로 사용해보자

--- src/server.v2/src/app.controller.ts
+++ src/server.v2/src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getFiles(@Res() res: Response): Promise<void> {
+    const htmlPath = path.join(__dirname, '..', '..', 'dist', 'index.html');
+    try {
+      const html = await fs.promises.readFile(htmlPath, 'utf-8');
+      res.setHeader('Content-Type', 'text/html');
+      res.send(html);
+    } catch (err) {
+      res.status(500).send('Internal Server Error');
+      console.log('Error occured : ', err);
+    }
+  }
+
+  @Get('bundle.js')
+  async getJSFile(@Res() res: Response): Promise<void> {
+    const JSPath = path.join(__dirname, '..', '..', 'dist', 'bundle.js');
+
+    try {
+      const js = await fs.promises.readFile(JSPath, 'utf-8');
+      res.setHeader('Content-Type', 'application/javascript');
+      res.send(js);
+    } catch (err) {
+      res.status(500).send('Internal Server Error');
+      console.log('Error occured : ', err);
+    }
   }
 }
