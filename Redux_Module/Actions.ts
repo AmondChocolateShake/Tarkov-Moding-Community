960bcc153585418716432b6954cde5c5667b6303
AmondChocolateShake <dongju230130@gmail.com>
2023-08-20 17:33:32 +0900

    Redux 사용 Assignees: Labels:  #6 Actions 파일에서 인터페이스 생성. typeof 를 활용해 자료형 검사 및 확장성 용이한 예제 적용

---
+++ Redux_Module/Actions.ts
@@ -0,0 +1,13 @@
+import { INCREMENT,DECREMENT, SELECTED } from "./Types";
+
+export interface IncrementAction{
+    type: typeof INCREMENT
+}
+
+export interface DecrementAction{
+    type: typeof DECREMENT
+}
+
+export interface SelectAction{
+    type: typeof SELECTED
+}
