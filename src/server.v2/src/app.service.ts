c644bcb971ebf692627d5ff8a76fea47763d7539
AmondChocolateShake <dongju230130@gmail.com>
2023-08-28 17:21:11 +0900

    의존성 주입을 어떻게 주고 MVC로 나눌지 #14 총기 관련 데이터 서빙은 gun에서 + eslint 수정하였으니 원상복구는 .eslintrc 파일 내  주석 해제

--- src/server.v2/src/app.service.ts
+++ src/server.v2/src/app.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+    async serveHTML() {
+        const htmlPath = path.join(__dirname, '..', '..', 'dist', 'index.html');
+        const html = await fs.promises.readFile(htmlPath, 'utf-8');
+        return html;
+    }
+
+}
