3dcac05e552e14ae341a09d653100d3a3377f207
AmondChocolateShake <dongju230130@gmail.com>
2023-09-01 13:06:18 +0900

    서버단 총기 프리셋 데이터 리스트 반환 처리 #16 서비스 쪽 fetch 코드 작성중

--- src/server.v2/src/gun/gun.service.ts
+++ src/server.v2/src/gun/gun.service.ts
@@ -3,4 +3,23 @@
 
 export class GunService {
     
+
+    toGetPreset(){
+        const query=`{
+            items() {
+                id
+                name
+                shortName
+            }
+        }`
+
+        fetch('https://api.tarkov.dev/graphql',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify()
+            })
+    }
 }
