4726c58512e8a3986e55c9e43a441ed4996c17ed
AmondChocolateShake <dongju230130@gmail.com>
2023-09-10 17:49:07 +0900

    reducer 추가 완료

--- redux/counterSlice.ts
+++ redux/counterSlice.ts
@@ -12,7 +12,18 @@ const pageSlice=createSlice({
     name:"pageHandler",
     initialState,
     reducers:{
-        
+        goToMain:(state)=>{
+            state.page="main"
+        },
+        goToPosts:(state)=>{
+            state.page="posts"
+        },
+        goToPosting:(state)=>{
+            state.page="posting"
+        },
+        viewPost:(state)=>{
+            state.page="viewPost"
+        }
     }
 })
 
