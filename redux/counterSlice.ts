4b8cba4eb555181a49ba89ce8f21ddf9bf9fd222
AmondChocolateShake <dongju230130@gmail.com>
2023-09-10 17:40:43 +0900

    slice 만드는 중 -> reducers 내에 필요한 동작 추가

--- redux/counterSlice.ts
+++ redux/counterSlice.ts
@@ -1,5 +1,19 @@
 import {createSlice} from '@reduxjs/toolkit'
 
+type Init={
+    page:string
+}
 
+const initialState:Init={
+    page:'main'
+}
+
+const pageSlice=createSlice({
+    name:"pageHandler",
+    initialState,
+    reducers:{
+        
+    }
+})
 
 
